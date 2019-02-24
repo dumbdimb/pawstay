0f88113ad70bc524ff0addbcca3d927903d80cd1
Jeremy Ma <jyaoma@gmail.com>
2019-02-24 07:33:12 -0600

    Fixes apostrophe problem in HomePage

--- src/scripts/components/HomePage.js
+++ src/scripts/components/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
     return <div className="home">
         <div className="home__hero-image"/>
-        <div className="text home__title">We are the warm stay for your dog<br/>when you're away.</div>
+        <div className="text home__title">We are the warm stay for your dog<br/>when you&apos;re away.</div>
         <div className="home__body">
         Hi! We are Clarissa and Darby, a human and a dog who believe in a better way of dog sitting. Pawstay is a personally-owned and home-based dog boarding and daycare. Here, your dog will be treated as family, well taken care of, pampered with cuddles and belly rubs, and have a time full of love.
         <br/>
